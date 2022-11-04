88504296d1e136312bb1ab4e3d8a04da435938eb
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-11-04 12:16:55 +0000

    ornek3

--- main.cpp
+++ main.cpp
@@ -5,34 +5,48 @@ class nokta {
 public:
   int x, y;
 
-  nokta();
+  nokta(){};
   nokta(int a, int b) {
     x = a;
     y = b;
   }
 
-  nokta operator+(nokta &bir) {
+  nokta operator +(nokta &bir) {
     nokta a;
     a.x = x + bir.x;
-    a.y = y + bir.x;
+    a.y = y + bir.y;
     return a;
   }
 
   nokta operator+(int c) {
-    this->x = x + c;
-    this->y = y + c;
+    x = x + c;
+    y = y + c;
     return *this;
+  }
+
+  bool operator>(nokta &object) {
+    if (x > object.x && y > object.y)
+    {
+      return true;
+    }
+    return false;
   }
 
   void yaz() { cout << " " << y; }
 };
 
 int main() {
-  nokta x(1, 5), y(8, 4);
+  nokta x(1, 5), y(8, 4),z(1,5);
   x = x + y;
-  x.yaz();
+  /*x.yaz();
   x = x + 8;
   x.yaz();
+  z=y+z+5;
+  z.yaz();*/
+  if (x>y)
+  {
+    cout << "x>y";
+  }
   
 
   return 0;
