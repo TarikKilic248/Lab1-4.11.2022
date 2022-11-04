0f0c70d79fc004927212e598b8bface56df13227
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-11-04 11:58:57 +0000

    calssları overloading yaparak toplama yaptık parametreli ve parametresiz constructor farkını gördük

--- .ccls-cache/@home@runner@Lab1-4112022/main.cpp
+++ .ccls-cache/@home@runner@Lab1-4112022/main.cpp
@@ -1,41 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class sayi {
+class nokta {
 public:
-  int a[10];
-  static int sta;
-  sayi &ekle(int value, int index) {
-    a[index] = value;
-    return *this;
+  int x, y;
+
+  nokta();
+
+  nokta(int a, int b) {
+    x = a;
+    y = b;
   }
 
-  void yaz() {
-    for (int i = 0; i < 10; i++) {
-      cout << a[i] << " ";
-    }
-    cout << endl;
+  nokta operator+(nokta &bir) {
+    nokta a;
+    a.x = x + bir.x;
+    a.y = y + bir.x;
+    return a;
   }
+
+  void yaz() { cout << " " << y; }
 };
 
-int sayi::sta;
+int main() {
+  nokta x(1, 5), y(1, 1);
+  x = x + y;
+  x = x + 8;
 
-int main() { // x = 359
-  sayi x;
-  sayi y;
-  x.ekle(3, 0).ekle(5, 1).ekle(9, 2);
-  x.yaz();
-
-  cout << "x " << x.sta << endl;
-
-  x.sta = 18;
-
-  cout << "x " << x.sta << endl;
-  cout << "y " << y.sta << endl;
-
-  y.sta = 122;
-
-  cout << "x " << x.sta << endl;
-  cout << "y " << y.sta << endl;
   return 0;
 }
